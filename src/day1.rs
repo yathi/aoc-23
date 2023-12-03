88e085880ce2a0ed16d69fd35ba78f11e5266273
yathi <bm.thunderemperor@gmail.com>
2023-12-02 19:52:31 -0500

    day1-part2

--- src/day1.rs
+++ src/day1.rs
@@ -1,15 +1,41 @@
 use std::fs;
 
 pub fn part2() {
-    let path = "data/day1-2-sample.txt";
-    // let path = "data/day1-2.txt";
+    // let path = "data/day1-2-sample.txt";
+    let path = "data/day1-1.txt";
     let contents = fs::read_to_string(path)
         .expect("Should have been able to read the file");
 
-    // let mut sum = 0;
+    let mut sum = 0;
+    let nums = ["one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9"];
     for line in contents.lines() {
-      println!("{:?}", line);
+        'out: for i in 0..line.len() {
+            for (index, num) in nums.iter().enumerate() {
+                if i + num.len() > line.len() {
+                    continue;
+                }
+                if &line[i..i+num.len()] == *num {
+                    let first = (1+index/2);
+                    sum += first*10;
+                    break 'out;
+                }
+            }
+        }
+        'out: for i in (0..line.len()).rev() {
+            for (index, num) in nums.iter().enumerate() {
+                if i + num.len() > line.len() {
+                    continue;
+                }
+                if &line[i..i+num.len()] == *num {
+                    let last = (1+index/2);
+                    sum += last;
+                    break 'out;
+                }
+            }
+        }
+    //   println!("{:?}", line);
     }
+    println!("{:?}", sum);
 }
 
 pub fn part1() {
