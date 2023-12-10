99154b22444ee68cc6f627aa46438f48f03054bf
yathi <bm.thunderemperor@gmail.com>
2023-12-09 22:53:17 -0500

    Day2-part2: First proper use of Hashmap in Rust

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 pub fn part1() {
@@ -40,4 +41,37 @@ fn is_valid_cube(color: &str, count: i32) -> bool {
         "green" => count <= 13,
         _ => panic!("Unexpected color"),
     }
+}
+
+pub fn part2() {
+    let path = "data/day2.txt";
+    let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+
+    let mut sum = 0;
+    for line in contents.lines() {
+        let (_, game) = line.split_once(": ").unwrap();
+        sum += get_power(game);
+    }
+    println!("{:?}", sum);
+}
+
+fn get_power(game: &str) -> i32 {
+    let sets = game.split("; ").collect::<Vec<_>>();
+    let mut map = HashMap::new();
+    for set in sets {
+        let pulls = set.split(", ").collect::<Vec<_>>();
+        // println!("{:?}", pulls);
+        for pull in pulls {
+            // println!("{:?}", pull);
+            let (count, color) = pull.split_once(" ").unwrap();
+            let count = count.parse::<i32>().unwrap();
+            let color_count = map.entry(color).or_insert(0);
+            *color_count = Ord::max(*color_count, count);
+        }
+    }
+    let mut power = 1;
+    for (_, count) in map.into_iter() {
+        power *= count;
+    }
+    power
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ pub mod day2;
 fn main() {
     // day1::part1();
     // day1::part2();
-    day2::part1();
+    // day2::part1();
+    day2::part2();
 }
