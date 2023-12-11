62e51f81cd891724c6a7a7f6cb70cb72afd01bf8
yathi <bm.thunderemperor@gmail.com>
2023-12-11 00:20:35 -0500

    Day3. Progress. Using structs.

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,70 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
-pub fn part1() {}
+pub fn part1() {
+    let path = "data/day3-sample.txt";
+    let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+
+    let mut sum = 0;
+    let mut parts = Vec::new();
+    let mut symbols: HashSet<(i32, i32)> = HashSet::new();
+    let mut curr_part_number: Option<PartNumber> = None;
+    for (row, line) in contents.lines().enumerate() {
+        println!("{:?}", line);
+        for (col, ch) in line.chars().enumerate() {
+            if ch.is_ascii_digit() {
+                if let Some(ref mut part_number) = curr_part_number {
+                    part_number.add_digit(row as i32, col as i32, ch);
+                } else {
+                    curr_part_number = Some(PartNumber::new(row as i32, col as i32, ch));
+                }
+                // its a part number
+            } else {
+                if let Some(part_number) = curr_part_number.take() {
+                    parts.push(part_number);
+                }
+                if ch == '.' {
+                    continue;
+                }
+                symbols.insert((row.try_into().unwrap(), col.try_into().unwrap()));
+            }
+        }
+    }
+    println!("{:?}", parts);
+    println!("{:?}", symbols);
+}
+
+#[derive(Debug)]
+struct PartNumber {
+    number: i32,
+    surrounding: HashSet<(i32, i32)>,
+}
+
+impl PartNumber {
+    fn new(row: i32, col: i32, ch: char) -> Self {
+        let surrounding = HashSet::from([
+            (row - 1, col - 1),
+            (row, col - 1),
+            (row + 1, col - 1), // left column
+            (row - 1, col),
+            (row + 1, col), // middle column
+            (row - 1, col + 1),
+            (row, col + 1),
+            (row + 1, col + 1), // right column
+        ]);
+        Self {
+            number: ch.to_digit(10).unwrap() as i32,
+            surrounding,
+        }
+    }
+
+    fn add_digit(&mut self, row: i32, col: i32, ch: char) {
+        self.number = self.number * 10 + ch.to_digit(10).unwrap() as i32;
+        self.surrounding.extend([
+            (row - 1, col + 1),
+            (row, col + 1),
+            (row + 1, col + 1), // right column
+        ]);
+    }
+}
+
 pub fn part2() {}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod day1;
 pub mod day2;
+pub mod day3;
 
 fn main() {
     // day1::part1();
     // day1::part2();
     // day2::part1();
-    day2::part2();
+    // day2::part2();
+    day3::part1();
 }
