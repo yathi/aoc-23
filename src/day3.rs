b7e28ac3418a8c9672b556cbb7970c83ee415a87
yathi <bm.thunderemperor@gmail.com>
2023-12-13 21:26:29 -0500

    Day3 Part2

--- src/day3.rs
+++ src/day3.rs
@@ -70,4 +70,41 @@ impl PartNumber {
     }
 }
 
-pub fn part2() {}
+pub fn part2() {
+    let path = "data/day3.txt";
+    let contents = fs::read_to_string(path).expect("Should have been able to read the file");
+    let mut parts = Vec::new();
+    let mut symbols: HashSet<(i32, i32)> = HashSet::new();
+    let mut curr_part_number: Option<PartNumber> = None;
+    for (row, line) in contents.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch.is_ascii_digit() {
+                if let Some(ref mut part_number) = curr_part_number {
+                    part_number.add_digit(row as i32, col as i32, ch);
+                } else {
+                    curr_part_number = Some(PartNumber::new(row as i32, col as i32, ch));
+                }
+                // its a part number
+            } else {
+                if let Some(part_number) = curr_part_number.take() {
+                    parts.push(part_number);
+                }
+                if ch == '*' {
+                    symbols.insert((row.try_into().unwrap(), col.try_into().unwrap()));
+                }
+            }
+        }
+    }
+    let mut sum = 0;
+    for symbol in symbols {
+        let gear_parts = parts
+            .iter()
+            .filter(|part| part.surrounding.contains(&symbol))
+            .collect::<Vec<_>>();
+
+        if gear_parts.len() == 2 {
+            sum += gear_parts.iter().map(|part| part.number).product::<i32>();
+        }
+    }
+    println!("{:?}", sum);
+}
