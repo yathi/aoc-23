1d804534c70b0dbf4f06df2dc8e642784ee3f85b
Yathi Manavalan <yathirajan.brammadesam-manavalan@instacart.com>
2024-01-03 23:29:35 -0500

    WIP

--- src/day6.rs
+++ src/day6.rs
@@ -1,8 +1,9 @@
 use std::fs;
 
 pub fn part1() {
-    // let path = "data/day5-sample.txt";
-    let path = "data/day5.txt";
+    let path = "data/day6-s.txt";
+    // let path = "data/day6.txt";
     let contents = fs::read_to_string(path).expect("Should have been able to read the file");
-    println!("{:?}", contents);
+    let (times, distances) = contents.split_once("\n").unwrap();
+    println!("{:?}", times.split_whitespace().collect::<Vec<_>>());
 }
